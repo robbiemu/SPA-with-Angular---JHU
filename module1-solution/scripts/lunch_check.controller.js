719367a8091bda2f60405a21a6ccca0a7dd5863c
Robisinho <roberto.tomas.cuentas@gmail.com>
2016-12-23 19:13:34 -0500

    regex correction
    I had a simple regex in place. changed to a more exact regex

--- module1-solution/scripts/lunch_check.controller.js
+++ module1-solution/scripts/lunch_check.controller.js
@@ -17,7 +17,7 @@
 
 Implement this case item 1, item2,,item3 or this case item 1, item2, ,item3 as not counting an 'empty' item towards the count of how many items there are in the list. Please make sure to put a comment somewhere next to the input textbox stating that you do NOT consider and empty item, i.e., , , as an item towards to the count, so whoever is grading your assignment doesn't erroneously mark that as an error.
             */
-            let rx = /([^\,]+)/g;
+            let rx = /(\s*?[^\,\s]+(?:\s[^\,])*\s*?)/g;
             $scope.noFoods = ($scope.rawFoods.match(rx) || []).length
         }
 
